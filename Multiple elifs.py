39e78a020b42d08b17b82b9a0fee5bf43155cf03
Siddhartha Thirupathi <128659991+SIDDHARTHA-TVS@users.noreply.github.com>
2023-03-23 12:55:21 +0530

    Multiple elifs

---
+++ Multiple elifs.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+color = input("What's your favorite color?")
+if color == 'purple':
+    print("Excellent choice")
+elif color == 'blue':
+    print("Sky Lover")
+elif color == 'white':
+    print("peaceful")
+elif color == 'yellow':
+    print("rocking")
+else:
+    print("unique")
+
