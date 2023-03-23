9518eb87ea4e3164a71dae051b349fe88cdfcbba
Siddhartha Thirupathi <128659991+SIDDHARTHA-TVS@users.noreply.github.com>
2023-03-23 16:09:27 +0530

    Logical Operators And, Or, Not

---
+++ Logical operators And, Or, Not.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+age = 6
+if age > 2 and age < 8:
+    print("You pay child ticket")
+else:
+    print("No need of child ticket"
+
+
+# In[ ]:
+
+
+city = input("Where do you live?")
+if city == "kakinada" or city == "vizag":
+    print("You live in Andhra Pradesh")
+else:
+    print("You live somewhere else!")
+
+
+# In[ ]:
+
+
+age = 7
+if not ((age >= 2 and age <= 8) or age >= 65):
+    print("You have to pay money")
+else:
+    print("You will get a free ticket")
+
+
+# In[3]:
+
+
+myage = 10
+
+
+# In[4]:
+
+
+age < 6
+
+
+# In[5]:
+
+
+not age < 6
+
+
+# In[ ]:
+
+
+
+
