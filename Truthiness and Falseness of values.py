35a213d76fae0b3fd0e43f374bcec25db7bdab28
Siddhartha Thirupathi <128659991+SIDDHARTHA-TVS@users.noreply.github.com>
2023-03-23 13:27:28 +0530

    Truthiness and Falseness of values

---
+++ Truthiness and Falseness of values.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+animal = input("What's your favorite animal?")
+if animal:
+    print(animal +" is my favorite too.")
+else:
+    print("You didn't say anything!")
+
