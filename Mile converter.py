6addbf1b228fd69370a6f2c26119af66cb9c16cc
SIDDHARTHA-TVS <128659991+SIDDHARTHA-TVS@users.noreply.github.com>
2023-03-23 10:01:15 +0530

    Mile converter

---
+++ Mile converter.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+print("How many kilometers did you run today?")
+kms = input()
+miles = float(kms)/1.609343502101154
+t = ("That is equal to {} miles")
+print(t.format(round(miles, 2)))
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
