0d067754b6524530ecb5ea2126fcba4158d80edb
Siddhartha Thirupathi <128659991+SIDDHARTHA-TVS@users.noreply.github.com>
2023-03-24 23:21:14 +0530

    Len_of_char, Adding 2-Digit num, Calculating BMI

---
+++ Len_of_char, Adding 2-Digit num, BMI.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+name = "TVSSIDDHARTHA"
+len(name)
+
+
+# In[ ]:
+
+
+
+
+
+# In[35]:
+
+
+num_char = len(input("What is your name?\n"))
+new_num_char = str(name_char)
+print("Your name has " + new_num_char + "characters.")
+
+
+# In[36]:
+
+
+x = input("Enter a two digit number\n")
+first = int(x[0])
+second = int(x[1])
+print(first)
+print(second)
+result = first + second
+print(result)
+
+
+# In[55]:
+
+
+print("To know your BMI")
+weight = input("Enter your weight\n")
+height = input("Enter your height\n")
+BMI = int(weight)/float(height) ** 2
+BMI_1 = int(BMI)
+print(BMI_1)
+
+
+# In[ ]:
+
+
+
+
