9b27e07e249786383f9a35703967c80416deca12
Siddhartha Thirupathi <128659991+SIDDHARTHA-TVS@users.noreply.github.com>
2023-03-25 00:33:15 +0530

    Tip Calculator

---
+++ Tip Calculator.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[13]:
+
+
+print("Welcome to Tip Calculator")
+bill = int(input("What was the total bill?\n"))
+tip = input("What percentage tip would you like to give? 10, 12, or 15\n")
+per = int(tip)
+TA = ((per/100) * bill)
+print(TA)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
