fc9597902733ed668e35517dce65ea2afdb9eddd
Siddhartha Thirupathi <128659991+SIDDHARTHA-TVS@users.noreply.github.com>
2023-03-25 00:20:35 +0530

    Age calculator

---
+++ Age left calculator (1).py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+Current = int(input("What's your current age?\n"))
+Left = 90 - Current
+x = int(Left)
+Y = int(x * 12)
+z = int(Y * 4)
+w = int(z * 365)
+a = "You are left with {}Years, {}months, {}weeks, {}days"
+print(a.format(x, Y, z, w))
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
---
+++ String Format (1).py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+a = 0
+b = 1.8
+c = True
+d = "I have a {} with a float of {} and the value is {}"
+print(d.format(a, b, c))
+
+
+# In[14]:
+
+
+Current = int(input("What's your current age?\n"))
+Left = 90 - Current
+x = int(Left)
+Y = int(x * 12)
+z = int(Y * 4)
+a = "You are left with {}Years, {}months, {}weeks"
+print(a.format(x, Y, z))
+
+
+
+# In[ ]:
+
+
+
+
